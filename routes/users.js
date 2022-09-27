39ab7cac2f0bce89ad5786c119364a4a0b696891
HenriqueFranca <henriqsf@gmail.com>
2022-09-27 09:12:44 -0300

    Refatorando o sistema e organizando o codigo

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,13 @@ let db = new NeDB({
 
 module.exports = (app)=>{
 
-    app.get('/users',(req, res)=>{
+    let route = app.route('/users');
+
+    app.get((req, res)=>{
 
         db.find({}).sort({name:1}).exec((err, users)=>{
             if(err){
-                console.log('error: ${err}');
-                res.status(400).json({
-                    error: err
-                });
+               app.utils.error.send(err, req,  res);
             } else{
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -34,14 +33,12 @@ module.exports = (app)=>{
     
     });
     
-    app.post('/users', (req, res)=>{
+    app.post((req, res)=>{
 
         db.insert(req.body, (err, user)=>{
             if(err){
-                console.log('error: ${err}');
-                res.status(400).json({
-                    error: err
-                });
+                app.utils.error.send(err, req,  res);
+               
             } else{
                 res.status(200).json(user);
             }
